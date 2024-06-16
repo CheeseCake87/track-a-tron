9bf52d26909194c2e66556321f53b379bfd8a015
David Carmichael <carmichaelits@gmail.com>
2024-06-16 12:18:48 +0100

    feat: add disabled to get address service settings

--- app/services/get_address/settings.py
+++ app/services/get_address/settings.py
@@ -1,11 +1,14 @@
 class GetAddressSettings:
     api_key: str
     administration_key: str
+    disabled: bool
 
     def __init__(
         self,
         api_key: str,
         administration_key: str,
+        disabled: bool,
     ):
         self.api_key = api_key
         self.administration_key = administration_key
+        self.disabled = disabled
