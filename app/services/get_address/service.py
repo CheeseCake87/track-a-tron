68ceb1163b8ca9d34b4a066b21a50c13cf6717cf
David Carmichael <carmichaelits@gmail.com>
2024-06-16 12:46:30 +0100

    feat: add disable check to get address service

--- app/services/get_address/service.py
+++ app/services/get_address/service.py
@@ -22,70 +22,94 @@ class GetAddressService:
             self.settings = self._load_service_settings()
 
     def find(self, postcode: str) -> dict:
-        resp = r.get(
-            self.base_url.format(postcode=postcode, api_key=self.settings.api_key)
-        )
+        if not self.settings.disabled:
+            resp = r.get(
+                self.base_url.format(postcode=postcode, api_key=self.settings.api_key)
+            )
 
-        if resp.status_code != 429:
-            with DBSession as s:
-                s.execute(
-                    query_create_system_log(
-                        "getAddress.io : Rate limit exceeded",
-                        "getAddress.io : Rate limit exceeded",
+            if resp.status_code != 429:
+                with DBSession as s:
+                    s.execute(
+                        query_create_system_log(
+                            "getAddress.io : Rate limit exceeded",
+                            "getAddress.io : Rate limit exceeded",
+                        )
                     )
-                )
-                s.commit()
+                    s.commit()
 
-            return {"ok": False, "message": "Rate limit exceeded"}
+                return {"ok": False, "message": "Rate limit exceeded"}
 
-        if resp.status_code != 200:
-            with DBSession as s:
-                s.execute(
-                    query_create_system_log(
-                        "getAddress.io : Error with request",
-                        resp.content.decode("utf-8"),
+            if resp.status_code != 200:
+                with DBSession as s:
+                    s.execute(
+                        query_create_system_log(
+                            "getAddress.io : Error with request",
+                            resp.content.decode("utf-8"),
+                        )
                     )
+                    s.commit()
+
+                return {"ok": False, "message": "Error with request"}
+
+            data = resp.json()
+
+            return {"ok": True, "data": data}
+
+        with DBSession as s:
+            s.execute(
+                query_create_system_log(
+                    "getAddress.io service is disabled",
+                    "getAddress.io service is disabled",
                 )
-                s.commit()
-
-            return {"ok": False, "message": "Error with request"}
-
-        data = resp.json()
-
-        return {"ok": True, "data": data}
+            )
+            s.commit()
+        return {"ok": False, "message": "Service is disabled"}
 
     def cache_find(self, postcode: str, refresh_cache: bool = False) -> dict:
-        if refresh_cache:
-            data = self.find(postcode)
+        if not self.settings.disabled:
+            if refresh_cache:
+                data = self.find(postcode)
 
-            if data.get("ok"):
-                with DBSession as s:
-                    result = s.execute(
-                        query_read_cache_entry(postcode)
-                    ).scalar_one_or_none()
-                    if result:
-                        s.execute(query_update_cache_entry(postcode, data))
+                if data.get("ok"):
+                    with DBSession as s:
+                        result = s.execute(
+                            query_read_cache_entry(postcode)
+                        ).scalar_one_or_none()
+                        if result:
+                            s.execute(query_update_cache_entry(postcode, data))
+                            return {"ok": True, "data": data}
+
+                        s.execute(query_create_cache_entry(postcode, data))
+                        s.commit()
                         return {"ok": True, "data": data}
 
+                return data
+
+            with DBSession as s:
+                result = s.execute(
+                    query_read_cache_entry(postcode)
+                ).scalar_one_or_none()
+                if result:
+                    return {"ok": True, "data": result.cache}
+
+                data = self.find(postcode)
+
+                if data.get("ok"):
                     s.execute(query_create_cache_entry(postcode, data))
                     s.commit()
                     return {"ok": True, "data": data}
 
-            return data
+                return data
 
         with DBSession as s:
-            result = s.execute(query_read_cache_entry(postcode)).scalar_one_or_none()
-            if result:
-                return {"ok": True, "data": result.cache}
-
-            data = self.find(postcode)
-
-            if data.get("ok"):
-                s.execute(query_create_cache_entry(postcode, data))
-                s.commit()
-                return {"ok": True, "data": data}
-
-            return data
+            s.execute(
+                query_create_system_log(
+                    "getAddress.io service is disabled",
+                    "getAddress.io service is disabled",
+                )
+            )
+            s.commit()
+        return {"ok": False, "message": "Service is disabled"}
 
     def _load_service_settings(self) -> GetAddressSettings:
         with DBSession as s:
