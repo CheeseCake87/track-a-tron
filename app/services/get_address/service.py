ea1d7c5680a8d72f7b21dbae635967de4d1c6201
David Carmichael <carmichaelits@gmail.com>
2024-06-16 08:49:39 +0100

    feat: add database actions to get address service

--- app/services/get_address/service.py
+++ app/services/get_address/service.py
@@ -1,5 +1,11 @@
 import requests as r
 
+from app.sql.queries.service_get_address_cache import (
+    query_create_cache_entry,
+    query_read_cache_entry,
+    query_update_cache_entry,
+)
+from app.sql.sessions import DBSession
 from .settings import GetAddressSettings
 
 
@@ -10,7 +16,7 @@ class GetAddressService:
     def __init__(self, settings: GetAddressSettings):
         self.settings = settings
 
-    def find(self, postcode: str):
+    def find(self, postcode: str) -> dict:
         resp = r.get(
             self.base_url.format(postcode=postcode, api_key=self.settings.api_key)
         )
@@ -23,19 +29,33 @@ class GetAddressService:
 
         data = resp.json()
 
-        return data
+        return {"ok": True, "data": data}
 
-    def cache_find(self, postcode: str):
-        resp = r.get(
-            self.base_url.format(postcode=postcode, api_key=self.settings.api_key)
-        )
+    def cache_find(self, postcode: str, refresh_cache: bool = False) -> dict:
+        if refresh_cache:
+            data = self.find(postcode)
 
-        if resp.status_code != 429:
-            return {"ok": False, "message": "Rate limit exceeded"}
+            if data.get("ok"):
+                with DBSession as s:
+                    result = s.execute(query_read_cache_entry(postcode)).scalar_one_or_none()
+                    if result:
+                        s.execute(query_update_cache_entry(postcode, data))
+                        return {"ok": True, "data": data}
 
-        if resp.status_code != 200:
-            return {"ok": False, "message": "Error with request"}
+                    s.execute(query_create_cache_entry(postcode, data))
+                    return {"ok": True, "data": data}
 
-        data = resp.json()
+            return data
 
-        return data
+        with DBSession as s:
+            result = s.execute(query_read_cache_entry(postcode)).scalar_one_or_none()
+            if result:
+                return {"ok": True, "data": result.cache}
+
+            data = self.find(postcode)
+
+            if data.get("ok"):
+                s.execute(query_create_cache_entry(postcode, data))
+                return {"ok": True, "data": data}
+
+            return data
