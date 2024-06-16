7b7cd80e2aaaa1ff0d9a5afa819589390046e1b8
David Carmichael <carmichaelits@gmail.com>
2024-06-16 10:44:16 +0100

    feat: remove dev_mode from zepto service

--- app/services/zepto/settings.py
+++ app/services/zepto/settings.py
@@ -1,22 +1,20 @@
 import typing as t
 
-TRULY = [1, "1", True, "True", "true"]
-
 
 class ZeptoSettings:
-    dev_mode: bool
     sender: str
     api_url: str
     token: str
+    disabled: bool
 
     def __init__(
         self,
-        dev_mode: t.Union[int, str, bool],
         sender: str,
         api_url: str,
         token: str,
+        disabled: t.Optional[bool] = False,
     ):
-        self.dev_mode = True if dev_mode in TRULY else False
         self.sender = sender
         self.api_url = api_url
         self.token = token
+        self.disabled = disabled
