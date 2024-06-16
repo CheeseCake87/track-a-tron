5c4a0f378a85bbbcd4d68c5023291de8eede6995
David Carmichael <carmichaelits@gmail.com>
2024-06-16 13:15:57 +0100

    feat: move security to utilities

--- app/rpc/user/funcs/update_user.py
+++ app/rpc/user/funcs/update_user.py
@@ -1,6 +1,6 @@
 from quart_rpc.version_1_0 import RPCResponse
 
-from app.rpc.security import session_check
+from app.utilities.security import session_check
 from app.sql import DBSession
 from app.sql.queries.user import (
     query_update_user,
