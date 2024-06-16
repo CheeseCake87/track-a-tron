a337f50d33b7d96288e6b6b8aebf0c3f6cea41dc
David Carmichael <carmichaelits@gmail.com>
2024-06-16 13:16:10 +0100

    feat: add services rpc

---
+++ app/rpc/system/blueprint.py
@@ -0,0 +1,14 @@
+from quart import Blueprint
+from quart_rpc.version_1_0 import RPC, RPCResponse
+
+from .funcs import __all__ as funcs
+
+system = Blueprint("system", __name__, url_prefix="/system")
+rpc = RPC(system)
+rpc.functions_auto_name(funcs)
+
+
+@system.errorhandler(404)
+@system.errorhandler(405)
+def route_404(_):
+    return RPCResponse.fail("Page or method not found.")
