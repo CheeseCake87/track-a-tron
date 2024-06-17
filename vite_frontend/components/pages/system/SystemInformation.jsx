669d8c162292343e2c01710261f64283b611e471
David Carmichael <carmichaelits@gmail.com>
2024-06-17 15:22:05 +0100

    feat: updated system information

--- vite_frontend/components/pages/system/SystemInformation.jsx
+++ vite_frontend/components/pages/system/SystemInformation.jsx
@@ -15,7 +15,7 @@ export default function SystemInformation() {
     return (
         <div className={'px-4'}>
             <h1 className={'m-0 pb-2'}>🤖Track-a-tron</h1>
-            <p className={'font-bold'}>Track-a-tron is a simple CRM system for tracking things.</p>
+            <p className={'font-bold'}>Track-a-tron is a simple system for tracking things.</p>
             <small>Version: 1000</small>
             <p className={'pt-2'}>View the project on <a
                 href="https://github.com/CheeseCake87/track-a-tron">GitHub</a></p>
