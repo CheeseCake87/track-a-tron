e72b2be9270e715da6d92135e5b526a4d1232efe
David Carmichael <carmichaelits@gmail.com>
2024-08-02 07:36:56 +0100

    feat: remove blank css file, fix icons and text when no tickets or clients are found

--- frontend/components/pages/clients/Clients/ClientsNoClients.jsx
+++ frontend/components/pages/clients/Clients/ClientsNoClients.jsx
@@ -1,15 +1,12 @@
 import {NoClientsIcon} from "../../../globals/Icons";
-import {ContextClients} from "../../../../contextManagers/ContextClients";
-import {useContext} from "solid-js";
 
 
 export default function ClientsNoClients() {
-    const ctxClients = useContext(ContextClients)
     return (
         <div className={'flex justify-center items-center w-full h-full min-h-0'}>
             <div className={'flex flex-col items-center'}>
                 <NoClientsIcon size={64}/>
-                <small>No tickets found</small>
+                <small>No clients found</small>
             </div>
         </div>
     )
--- frontend/components/pages/workshop/Workshop/WorkshopNoTickets.jsx
+++ frontend/components/pages/workshop/Workshop/WorkshopNoTickets.jsx
@@ -1,14 +1,11 @@
-import {NoClientsIcon} from "../../../globals/Icons";
-import {ContextWorkshop} from "../../../../contextManagers/ContextWorkshop";
-import {useContext} from "solid-js";
+import {WorkshopIcon} from "../../../globals/Icons";
 
 
 export default function WorkshopNoTickets() {
-    const ctxWorkshop = useContext(ContextWorkshop)
     return (
         <div className={'flex justify-center items-center w-full h-full min-h-0'}>
             <div className={'flex flex-col items-center'}>
-                <NoClientsIcon size={64}/>
+                <WorkshopIcon size={64}/>
                 <small>No tickets found</small>
             </div>
         </div>
