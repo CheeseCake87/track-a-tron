0a544c2eb1e61332d54b348373743290b2778d6c
David Carmichael <carmichaelits@gmail.com>
2024-08-14 13:43:10 +0100

    feat: remove loading message

--- frontend/components/pages/clients/Clients/ClientsLoading.jsx
+++ frontend/components/pages/clients/Clients/ClientsLoading.jsx
@@ -4,7 +4,7 @@ import {SpinnerWithMessage} from "../../../globals/Spinner";
 export default function ClientsLoading() {
     return (
         <div className={'flex justify-center items-center w-full h-full min-h-0'}>
-            <SpinnerWithMessage message="loading..."/>
+            <SpinnerWithMessage/>
         </div>
     )
 }
